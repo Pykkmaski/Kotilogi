51628c8ff9b183f3dbbdc851371ac2862088acdb
unknown <jens.osterberg@gmail.com>
2023-05-12 13:21:56 +0300

    Fixed properties page header not showing up when user has no properties yet

--- src/Hooks/useProperties.js
+++ src/Hooks/useProperties.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import AppContext from '../Contexts/AppContext';
 
 function useProperties(){
-    const [properties, setProperties] = useState(null);
+    const [properties, setProperties] = useState([]);
 
     function loadProperties(){
         //Implicitly fetches the properties belonging to the logged in user, by verifying the authorization token sent in the header.
