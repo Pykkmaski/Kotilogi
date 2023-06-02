ec0ba8029dc1bfab2285039daa5eb4f7153fec36
unknown <jens.osterberg@gmail.com>
2023-06-02 12:55:02 +0300

    fix: wrong date variable name

--- src/Pages/Event/Header.js
+++ src/Pages/Event/Header.js
@@ -11,7 +11,7 @@ function Header(props){
     const [showModal, setShowModal] = useState(false);
     const mainImageUrl = `/api/images/events/image/${mainImageId}`;
 
-    const time = typeof(event.time) === 'string' ? parseInt(event.time) : event.time;
+    const time = typeof(event.date) === 'string' ? parseInt(event.date) : event.date;
     const date = new Date(time).toLocaleDateString('fi-FI');
     
     return (
