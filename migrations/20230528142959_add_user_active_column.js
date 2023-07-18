2913d8b6f0b219e5c6a5f0f50f0109e4d336b937
unknown <jens.osterberg@gmail.com>
2023-07-18 22:13:42 +0300

    Updated user active column migration file

--- migrations/20230528142959_add_user_active_column.js
+++ migrations/20230528142959_add_user_active_column.js
@@ -2,9 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+const tableName = 'users';
+const columnName = 'active';
+
 exports.up = function(knex) {
-  return knex.schema.table('users', tbl => {
-    tbl.boolean('active').defaultTo(false);
+  return knex.schema.table(tableName, tbl => {
+    tbl.boolean(columnName).defaultTo(false);
   });
 };
 
@@ -13,7 +17,7 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schea.table('users', tbl => {
-    tbl.dropColumn('active');
+  return knex.schea.table(tableName, tbl => {
+    tbl.dropColumn(columnName);
   });
 };
