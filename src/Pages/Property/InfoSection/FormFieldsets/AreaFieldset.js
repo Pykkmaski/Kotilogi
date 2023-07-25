c4d6dec7177a5947c35a9b3ee9fadb36758365e7
unknown <jens.osterberg@gmail.com>
2023-07-25 15:13:32 +0300

    Update: added step property

--- src/Pages/Property/InfoSection/FormFieldsets/AreaFieldset.js
+++ src/Pages/Property/InfoSection/FormFieldsets/AreaFieldset.js
@@ -20,12 +20,12 @@ function AreaFieldset(props){
             <Form.Legend>Asunnon pinta-alat (m<sup>2</sup>)</Form.Legend>
             <Form.Group>
                 <Form.Label>Asuintilojen pinta-ala</Form.Label>
-                <Form.Control id="living-area-input" type="number" name="living_area" defaultValue={property.living_area} onInput={(e) => livingArea.current = e.target.valueAsNumber}></Form.Control>
+                <Form.Control id="living-area-input" type="number" step={0.01} name="living_area" defaultValue={property.living_area} onInput={(e) => livingArea.current = e.target.valueAsNumber}></Form.Control>
             </Form.Group>
 
             <Form.Group>
                 <Form.Label>Muut tilat</Form.Label>
-                <Form.Control id="other-area-input" type="number" name="other_area" defaultValue={property.other_area} onInput={(e) => otherArea.current = e.target.valueAsNumber}></Form.Control>
+                <Form.Control id="other-area-input" type="number" step={0.01} name="other_area" defaultValue={property.other_area} onInput={(e) => otherArea.current = e.target.valueAsNumber}></Form.Control>
             </Form.Group>
 
             <Form.Group>
