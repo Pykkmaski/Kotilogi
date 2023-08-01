187f32a2c8deaa2721644c46c7c7bc50efcd996a
unknown <jens.osterberg@gmail.com>
2023-08-01 18:31:45 +0300

    update: index footer

--- src/Pages/Index/Footer.js
+++ src/Pages/Index/Footer.js
@@ -3,11 +3,20 @@ import ContactForm from "./ContactForm";
 function Footer(props){
     return (
         <div className="group-column" id="home-page-footer">
-            <header>
-                <h1>Ota Yhteyttä</h1>
-            </header>
+            
 
-            <ContactForm/>
+            <div className="footer-element">
+                <header>
+                    <h1>Ota Yhteyttä</h1>
+                </header>
+
+                <p>
+                    Kysyttävää? Lähetä meille viesti ja palaamme asiaan mahdollisimman pian.<br/>
+                    Antamaasi sähköpostia ei käytetä mihinkään muuhun kuin viestiisi vastaamiseen.
+                </p>
+
+                <ContactForm/>
+            </div>
         </div>
     )
 }
