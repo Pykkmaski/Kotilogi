432169f027d44da4cc8f6829d0acda104f88b445
unknown <jens.osterberg@gmail.com>
2023-09-04 15:18:46 +0300

    update: removed getServerSideProps

--- app/auth/properties/[property_id]/usage/page.tsx
+++ app/auth/properties/[property_id]/usage/page.tsx
@@ -1,16 +1,8 @@
 import db from 'kotilogi-app/dbconfig';
 import styles from './page.module.scss';
 import {serverGetDataById} from 'kotilogi-app/actions/serverGetData';
-import { getServerSession } from 'next-auth';
 import { PropertyType } from 'kotilogi-app/types/PropertyType';
 import { throwErrorIfNull } from 'kotilogi-app/utils/throwErrorIfNull';
-
-export async function getServerSideProps(){
-    const session = await getServerSession();
-    return {
-        props: {session}
-    }
-}
 
 export default async function UsagePage({params}){
     const property = await serverGetDataById(params.property_id, 'properties') as PropertyType | null;
