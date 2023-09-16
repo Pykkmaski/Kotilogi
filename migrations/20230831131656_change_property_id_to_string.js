b64cdf64f8a913be2644bd8b09fbf453d3e743fc
unknown <jens.osterberg@gmail.com>
2023-09-16 20:21:04 +0300

    update: integer to string method in mig

--- migrations/20230831131656_change_property_id_to_string.js
+++ migrations/20230831131656_change_property_id_to_string.js
@@ -9,8 +9,42 @@ const tableName = 'properties';
 const columnName = 'id';
 
 exports.up = async function(knex) {
-    return knex.schema.alterTable(tableName, tbl => {
-        tbl.string(columnName).alter();
+    return new Promise(async (resolve, reject) => {
+        try{
+            const oldIds = await knex(tableName).select('id');
+
+            //Remove constraints
+            await knex.schema.alterTable(tableName, tbl => {
+                tbl.dropPrimary();
+            });
+
+            //Create a new column
+            await knex.schema.alterTable(tableName, tbl => {
+                tbl.string('new_id').primary('PK_PROPERTIES').unique().notNullable().defaultTo(knex.fn.uuid());
+            });
+
+            //Copy data
+            for(const value of oldIds){
+                await knex(tableName).where({id: value.id}).update({
+                    new_id: value.id.toString(),
+                });
+            }
+
+            //Drop the old column
+            await knex.schema.alterTable(tableName, tbl => {
+                tbl.dropColumn(columnName);
+            })
+            .
+            //Rename the new column
+            alterTable(tableName, tbl => {
+                tbl.renameColumn('new_id', columnName);
+            });
+
+            resolve();
+        }
+        catch(err){
+            reject(err);
+        }
     });
 };
 
