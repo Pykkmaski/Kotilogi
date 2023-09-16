a575b16414ff45fcb9f0c104b7b865af40d843ee
unknown <jens.osterberg@gmail.com>
2023-09-16 19:59:51 +0300

    update: integers to strings method in migrations

--- migrations/20230831131656_change_property_id_to_string.js
+++ migrations/20230831131656_change_property_id_to_string.js
@@ -9,36 +9,8 @@ const tableName = 'properties';
 const columnName = 'id';
 
 exports.up = async function(knex) {
-    return new Promise(async (resolve, reject) => {
-        try{
-            const previousValues = await knex(tableName);
-            //Convert ids to strings
-            for(const value of previousValues){
-                value.id = value.id.toString();
-            }
-
-            //Drop the current id column
-            await knex.schema.alterTable(tableName, tbl => {
-                tbl.dropColumn(columnName);
-            });
-
-            //Create a new id column using strings as values
-            await knex.schema.alterTable(tableName, tbl => {
-                tbl.string(columnName).primary().unique();
-            });
-
-            //Repopulate the new string ids
-            for(const value of previousValues){
-                const query = {...value};
-                delete query.id;
-                await knex(tableName).where(query).update(value);
-            }
-
-            resolve();
-        }
-        catch(err){
-            reject(err);
-        }
+    return knex.schema.alterTable(tableName, tbl => {
+        tbl.string(columnName).alter();
     });
 };
 
@@ -47,41 +19,7 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function(knex) {
-   return new Promise(async (resolve, reject) => {
-    try{
-        const previousValues = await knex(tableName);
-
-        //Drop the old id column
-        await knex.schema.alterTable(tableName, tbl => {
-            tbl.dropColumn(columnName);
-        });
-
-        //Convert the string ids back to numbers.
-        for(const value of previousValues){
-            const newId = parseInt(value.id);
-            if(isNaN(newId)){
-                const max = 0xfffffffff;
-                value.id = crypto.randomInt(max);
-            }
-            else{
-                value.id = newId;
-            }
-        }
-
-        //Create new id column
-        await knex.schema.alterTable(tableName, tbl => {
-            tbl.dropColumn(columnName);
-        });
-
-        //Populate the new ids
-        for(const value of previousValues){
-            const query = {...value};
-            delete query.id;
-            await knex(tableName).where(query).update(value);
-        }
-    }
-    catch(err){
-        reject(err);
-    }
-   })
+    return knex.schema.alterTable(tableName, tbl => {
+        tbl.integer(columnName).alter();
+    });
 };
