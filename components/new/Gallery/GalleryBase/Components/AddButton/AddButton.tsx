4a6d116f9b76b78e79109d4dfa2da482ad5a5464
unknown <jens.osterberg@gmail.com>
2023-09-23 17:27:18 +0300

    Small mobile add button

--- components/new/Gallery/GalleryBase/Components/AddButton/AddButton.tsx
+++ components/new/Gallery/GalleryBase/Components/AddButton/AddButton.tsx
@@ -1,9 +1,16 @@
 import useGalleryContext from "../../GalleryContext";
+import style from './style.module.scss';
 
 export default function AddButton(){
     const {dispatch} = useGalleryContext();
 
     return (
-        <button className="primary add" type="button" onClick={() => dispatch({type: 'toggle_add_modal', value: true})}>Lisää Uusi</button>
+        <>
+            <button id={style.desktopAddButton} className="primary add" type="button" onClick={() => dispatch({type: 'toggle_add_modal', value: true})}>Lisää Uusi</button>
+            {/**Mobile devices */}
+            <div className={style.mobileAddButton} onClick={() => dispatch({type: 'toggle_add_modal', value: true})}>
+                <img src="/img/Icons/plus.png"></img>
+            </div>
+        </>
     );
 }
