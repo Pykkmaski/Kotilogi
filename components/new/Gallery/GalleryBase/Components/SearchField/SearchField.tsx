6ff7442dbb1a3e20bbb03c3af6d8d39628602eef
unknown <jens.osterberg@gmail.com>
2023-12-25 03:06:16 +0200

    Added timeout to searches

--- components/new/Gallery/GalleryBase/Components/SearchField/SearchField.tsx
+++ components/new/Gallery/GalleryBase/Components/SearchField/SearchField.tsx
@@ -1,15 +1,35 @@
+'use client';
+
+import { useEffect, useRef, useState } from "react";
 import useGalleryContext from "../../GalleryContext";
 
 export function SearchField(){
 
     const {dispatch, state} = useGalleryContext();
-
+    const [search, setSearch] = useState('');
+    const timeout = useRef<NodeJS.Timeout | null>(null);
+    
     const onInputHandler = (e) => {
-        dispatch({
-            type: 'set_search',
-            value: e.target.value,
-        });
+        setSearch(e.target.value);
     }
+
+    useEffect(() => {
+        const duration = 500;
+
+        timeout.current = setTimeout(() => {
+            console.log('Dispatching search...');
+            dispatch({
+                type: 'set_search',
+                value: search,
+            })
+    
+        }, duration);
+
+        return () => {
+            if(!timeout.current) return;
+            clearTimeout(timeout.current);
+        }
+    }, [search])
 
     return (
         <input type="search" placeholder="Etsi..." onChange={onInputHandler} disabled={state.data.length === 0}/>
