adeaf3d8076689f6a4dfab0553d7ebf34d34702f
unknown <jens.osterberg@gmail.com>
2023-12-29 20:40:02 +0200

    Moved index components into pages/index

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import WelcomeText from './_components/WelcomeText/WelcomeText';
-import AppDescription, { DescriptionElement } from './_components/AppDescription/AppDescription';
-import ProfileText from './_components/ProfileText/ProfileText';
-import Footer from './_components/Footer/Footer';
-import React from 'react';
-import Carousel from 'kotilogi-app/components/Carousel/Carousel';
 import style from './page.module.scss';
-import Pricing from './_components/Pricing/Pricing';
+import WelcomeText from 'kotilogi-app/components/Pages/Index/WelcomeText/WelcomeText';
+import ProfileText from 'kotilogi-app/components/Pages/Index/ProfileText/ProfileText';
+import Pricing from 'kotilogi-app/components/Pages/Index/Pricing/Pricing';
+import Footer from 'kotilogi-app/components/Pages/Index/Footer/Footer';
+
 
 export default function Page(props){
     return (
