b5c25ffc2b43fced405f4dcc69166bf028dc7e10
unknown <jens.osterberg@gmail.com>
2024-02-04 15:48:44 +0200

    Updated app loading.tsx to use tailwind styles

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import style from './loading.module.scss';
 
 export default function Loading(){
     return(
-        <div className={style.loading}>
+        <div className="flex flex-col items-center justify-center flex-1 text-slate-500">
             <Spinner size="2rem"/>
         </div>
     );
