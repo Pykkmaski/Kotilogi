d44cc60cf330d18f76e986d0c180760cc52615e2
unknown <jens.osterberg@gmail.com>
2024-02-04 19:49:15 +0200

    Updated Layout component to use tailwind styles

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import style from './style.module.scss';
 /**Renders the children with a pre-determined style required by the design of the page. */
 export function Layout({children}: React.PropsWithChildren){
     return (
-        <div className={style.container}>
+        <div className="flex flex-col px-32 flex-1 relative mt-8">
             {children}
         </div>
     );
