582aa9da117694584b984af595df2e8514e5bc1e
unknown <jens.osterberg@gmail.com>
2024-02-05 13:17:25 +0200

    Deleted Button2

--- components/Button/Button2.tsx
+++
@@ -1,5 +0,0 @@
-type ButtonProps = React.ComponentProps<'button'>;
-
-export function PrimaryButton({children, ...props}: ButtonProps){
-    return <button {...props} className="rounded-md p-2 text-black font-semibold bg-orange-300 shadow-md hover:bg-orange-200">{children}</button>
-}
