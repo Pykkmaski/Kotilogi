60dfa2d1b5e68ffcfc5eb9789d8734f2fd34db5a
unknown <jens.osterberg@gmail.com>
2024-02-07 17:16:24 +0200

    Fixed build errors

--- app/(auth)/dashboard/(dashboard layout)/settings/EmailSettingsForm.tsx
+++ app/(auth)/dashboard/(dashboard layout)/settings/EmailSettingsForm.tsx
@@ -4,23 +4,11 @@ import { updateEmail } from "kotilogi-app/actions/user/updateEmail";
 import { Input } from "kotilogi-app/components/Input/Input";
 import { SingleInputForm } from "kotilogi-app/components/SingleInputForm/SingleInputForm";
 import toast from "react-hot-toast";
-import {z} from 'zod';
-
-const EmailSchema = z.object({
-    email: z.string().email(),
-});
 
 export function EmailSettingsForm({email}){
 
     const submitMethod = async (newData: {email: string}) => {
-        const result = EmailSchema.safeParse(newData);
-
-        if(!result.success){
-            return Promise.reject(result.error.message);
-        }
-        else{
-            return updateEmail(email, newData.email);
-        }
+        return updateEmail(email, newData.email);
     }
 
     return (
