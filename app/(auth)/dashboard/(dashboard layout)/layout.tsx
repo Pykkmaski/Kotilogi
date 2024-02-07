ae8d2a00e8f84c8323354c6674d516407505115c
unknown <jens.osterberg@gmail.com>
2024-02-07 23:27:01 +0200

    Removed the billing link from the dashboard

--- app/(auth)/dashboard/(dashboard layout)/layout.tsx
+++ app/(auth)/dashboard/(dashboard layout)/layout.tsx
@@ -27,7 +27,6 @@ export default async function DashboardLayout({children}){
                     <NavBar>
                         <IconLink imageSrc="/icons/house.png" href="/dashboard/properties">Talot</IconLink>
                         <IconLink imageSrc="/icons/settings.png" href="/dashboard/settings">Asetukset</IconLink>
-                        <IconLink imageSrc="/icons/user.png" href="/dashboard/plan">Laskutus</IconLink>
                     </NavBar>
                 </div>
                 
