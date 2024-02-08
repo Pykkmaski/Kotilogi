8a85d2cac2935537b8c77af9ef5ca16a5b255b77
unknown <jens.osterberg@gmail.com>
2024-02-08 18:48:39 +0200

    Update usage page: Added total expenses beside the pie chart

--- app/(auth)/properties/[property_id]/usage/page.tsx
+++ app/(auth)/properties/[property_id]/usage/page.tsx
@@ -5,7 +5,7 @@ import { ContentCard } from '@/components/RoundedBox/RoundedBox';
 import { Group } from '@/components/Group';
 
 async function getUsageData(propertyId: string, type?: 'heat' | 'water' | 'electric'){
-    return new Promise<Record<string, string>[] | undefined>(async (resolve, reject) => {
+    return new Promise<Kotilogi.UsageType[] | undefined>(async (resolve, reject) => {
         try{
             const data = (
                 type ? await db('usage').where({type, refId: propertyId})
@@ -27,15 +27,24 @@ export default async function UsagePage({params, searchParams}){
     const allData = await getUsageData(params.property_id);
 
     if(!dataByType || !allData) throw new Error('Kulutustietojen lataus epäonnistui!');
-
+    
+    const totalPrice = allData.reduce((acc: number, cur) => acc += cur.price, 0);
     return (    
         <main>
             <Group direction="col" gap={4}>
                 <Content data={dataByType} type={type} />
                 <div className="w-full">
                     <ContentCard title="Yleiskatsaus">
-                        <div className="w-[500px]">
-                            <UsagePieChart data={allData}/>
+                        <div className="flex gap-4">
+                            <div className="w-[500px]">
+                                <UsagePieChart data={allData}/>
+                            </div>
+
+                            <div className="text-slate-500 flex flex-col">
+                                <span className="text-sm">Kuluihin käytetty yhteensä:</span>
+                                <h1 className="text-4xl">{totalPrice.toFixed(2)}€</h1>
+                            </div>
+                            
                         </div>
                     </ContentCard>
                 </div>
