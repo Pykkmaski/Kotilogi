2835271fc379bcb488b667ffdc25504ee12e6e12
unknown <jens.osterberg@gmail.com>
2024-02-08 21:55:45 +0200

    Updated app notice component

--- components/App/Notice.tsx
+++ components/App/Notice.tsx
@@ -1,7 +1,9 @@
 export default function Notice(props: {text: string}){
     return (
-        <div className="w-full z-40 fixed bottom-0 left-0 bg-red-500 text-center pt-2 pb-2 text-white font-semibold">
-            {props.text}
+        <div className="w-full z-40 fixed bottom-0 left-0 bg-red-500 text-center py-2 text-white font-semibold">
+            <span className="sm:text-sm lg:text-base">
+                {props.text}
+            </span>
         </div>
     );
 }
--- components/Util/Padding.tsx
+++ components/Util/Padding.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export function Padding({children}: PropsWithChildren){
-    return <div className="sm:px-4 lg:px-32">{children}</div>
+    return <div className="sm:px-2 lg:px-32">{children}</div>
 }
