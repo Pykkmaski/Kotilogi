b94a9a3c182229f3663f671921dc43ec19ac63d4
unknown <jens.osterberg@gmail.com>
2024-03-31 18:52:29 +0300

    Centered the footer contents on the about page

--- app/(transparentHeader)/about/Footer.tsx
+++ app/(transparentHeader)/about/Footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export function Footer(){
     return (
-        <footer className="flex flex-col gap-4 md:justify-between xs:justify-center bg-primary md:px-32 xs:px-4 pt-10 pb-32 w-full">
-            <h2 className="text-4xl font-semibold text-black w-full xs:text-center md:text-left">Kotidok Oy</h2>
-            <div className="flex flex-col gap-4 w-full md:items-start xs:items-center">
+        <footer className="flex flex-col gap-4 justify-center items-center bg-primary md:px-32 xs:px-4 pt-10 pb-32 w-full">
+            <h2 className="text-4xl font-semibold text-black xs:text-center md:text-left">Kotidok Oy</h2>
+            <div className="flex flex-col gap-4 md:items-start xs:items-center">
                 <Link href="/">Etusivulle</Link>
             </div>
         </footer>
