e98647bbb90585f5b080e56d0be824d6edd3f0de
unknown <jens.osterberg@gmail.com>
2024-04-02 18:44:38 +0300

    updated cart items component

--- app/(blackHeader)/(auth)/dashboard/(dashboard layout)/cart/CurrentCartItems.tsx
+++ app/(blackHeader)/(auth)/dashboard/(dashboard layout)/cart/CurrentCartItems.tsx
@@ -5,7 +5,7 @@ export function CurrentCartItems({items}){
         <div className="flex flex-col gap-2 flex-1">
             <h2 className="text-xl text-slate-500 font-semibold mb-4">Nykyiset talot</h2>
             {
-                items.map(item => <CartItem item={item}/>)
+                items?.map(item => <CartItem item={item}/>)
             }
         </div>
     );
