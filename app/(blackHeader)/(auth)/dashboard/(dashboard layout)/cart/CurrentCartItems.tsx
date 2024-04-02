7ccb8774f2dfcc20b0f11b2f871f9ba06bf11dd4
unknown <jens.osterberg@gmail.com>
2024-04-02 18:14:33 +0300

    Updated the current cart items header text

--- app/(blackHeader)/(auth)/dashboard/(dashboard layout)/cart/CurrentCartItems.tsx
+++ app/(blackHeader)/(auth)/dashboard/(dashboard layout)/cart/CurrentCartItems.tsx
@@ -3,7 +3,7 @@ import { CartItem } from "./CartItem";
 export function CurrentCartItems({items}){
     return (
         <div className="flex flex-col gap-2 flex-1">
-            <h2 className="text-xl text-slate-500 font-semibold mb-4">Nykyiset tuotteet</h2>
+            <h2 className="text-xl text-slate-500 font-semibold mb-4">Nykyiset talot</h2>
             {
                 items.map(item => <CartItem item={item}/>)
             }
