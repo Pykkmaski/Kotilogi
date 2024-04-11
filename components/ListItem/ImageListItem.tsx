3dad8355d6674d5f898326f669e3faa5f7693012
unknown <jens.osterberg@gmail.com>
2024-04-11 17:37:19 +0300

    Updated file and image list items

--- components/ListItem/ImageListItem.tsx
+++ components/ListItem/ImageListItem.tsx
@@ -1,39 +1,45 @@
-'use client';
+"use client";
 
 import { FileListItemProps } from "./FileListItem";
 import { ListItem, ListItemProps, ListItemProvider } from "./ListItem";
 import Link from "next/link";
-import {deleteFile as deletePropertyFile} from '@/actions/experimental/properties';
-import {deleteFile as deleteEventFile} from '@/actions/experimental/events';
+import { deleteFile as deletePropertyFile } from "@/actions/experimental/properties";
+import { deleteFile as deleteEventFile } from "@/actions/experimental/events";
 
-function ImageListItem(props: Omit<FileListItemProps, 'icon'>){
+function ImageListItem(props: Omit<FileListItemProps, "icon">) {
+  const imageSrc = `/api/files/${props.item.id}?tableName=${props.tablename}`;
 
-    const imageSrc = `/api/files/${props.item.id}?tableName=${props.tablename}`;
+  return (
+    <ListItemProvider item={props.item}>
+      <div className="relative aspect-square overflow-hidden object-contain rounded-lg lg:w-[200px] xs:w-[45%] flex items-center justify-center shadow-md">
+        <Link href={imageSrc} target="_blank">
+          <img style={{ objectFit: "contain" }} src={imageSrc}></img>
+        </Link>
 
-    return (
-        <ListItemProvider item={props.item}>
-            <div className="relative aspect-square overflow-hidden object-contain rounded-lg w-[200px] flex items-center justify-center shadow-md">
-                <Link href={imageSrc} target="_blank">
-                    <img style={{objectFit: 'contain'}} src={imageSrc}></img>
-                </Link>
-
-                <div className="absolute top-2 right-2 z-40 aspect-square w-5">
-                    <ListItem.CheckBox/>
-                </div>
-            </div>
-        </ListItemProvider>
-        
-    );
+        <div className="absolute top-2 right-2 z-40 aspect-square w-5">
+          <ListItem.CheckBox />
+        </div>
+      </div>
+    </ListItemProvider>
+  );
 }
 
-export function PropertyImageListItem(props: ListItemProps<Kotilogi.FileType>){
-    return (
-        <ImageListItem {...props} tablename="propertyFiles" deleteMethod={() => deletePropertyFile(props.item.id)}/>
-    );
+export function PropertyImageListItem(props: ListItemProps<Kotilogi.FileType>) {
+  return (
+    <ImageListItem
+      {...props}
+      tablename="propertyFiles"
+      deleteMethod={() => deletePropertyFile(props.item.id)}
+    />
+  );
 }
 
-export function EventImageListItem(props: ListItemProps<Kotilogi.FileType>){
-    return (
-        <ImageListItem {...props} tablename="eventFiles" deleteMethod={() => deleteEventFile(props.item.id)} />
-    );
+export function EventImageListItem(props: ListItemProps<Kotilogi.FileType>) {
+  return (
+    <ImageListItem
+      {...props}
+      tablename="eventFiles"
+      deleteMethod={() => deleteEventFile(props.item.id)}
+    />
+  );
 }
--- components/new/Gallery/GalleryBase/ImagesGallery.tsx
+++ components/new/Gallery/GalleryBase/ImagesGallery.tsx
@@ -1,8 +1,11 @@
-'use client';
+"use client";
 
 import AddFilesModal from "@/components/Experimental/Modal/AddFilesModal";
 import { ListItemProps } from "@/components/ListItem/ListItem";
-import { AddButton, DeleteButton } from "@/components/new/Gallery/GalleryBase/Buttons";
+import {
+  AddButton,
+  DeleteButton,
+} from "@/components/new/Gallery/GalleryBase/Buttons";
 import { ImageError } from "@/components/new/Gallery/GalleryBase/Components/Error/ImageError";
 import DeleteSelectedItemsModal from "@/components/new/Gallery/GalleryBase/DeleteSelectedItemsModal";
 import { Gallery } from "@/components/new/Gallery/GalleryBase/Gallery";
@@ -10,44 +13,58 @@ import { addFiles, deleteFile } from "kotilogi-app/actions/experimental/files";
 import { FileTableName } from "kotilogi-app/types/FileTableName";
 
 type ImagesGalleryProps = {
-    images: Kotilogi.FileType[];
+  images: Kotilogi.FileType[];
 
-    /**The id of the resource the images belong to, eg. the id of a property. */
-    refId: string;
+  /**The id of the resource the images belong to, eg. the id of a property. */
+  refId: string;
 
-    tablename: FileTableName;
+  tablename: FileTableName;
 
-    ImageComponent: React.FC<ListItemProps<Kotilogi.FileType>>;
-}
+  ImageComponent: React.FC<ListItemProps<Kotilogi.FileType>>;
+};
 
-export function ImagesGallery({tablename, images, refId, ImageComponent}: ImagesGalleryProps){
-    return (
-        <Gallery data={images}>
-            <Gallery.AddModal>
-                <AddFilesModal accept="image/jpeg" uploadMethod={async (fdata: FormData[]) => {
-                    await addFiles(tablename, fdata, refId);
-                }}/>
-            </Gallery.AddModal>
+export function ImagesGallery({
+  tablename,
+  images,
+  refId,
+  ImageComponent,
+}: ImagesGalleryProps) {
+  return (
+    <Gallery data={images}>
+      <Gallery.AddModal>
+        <AddFilesModal
+          accept="image/jpeg"
+          uploadMethod={async (fdata: FormData[]) => {
+            await addFiles(tablename, fdata, refId);
+          }}
+        />
+      </Gallery.AddModal>
 
-            <Gallery.DeleteModal>
-                <DeleteSelectedItemsModal deleteMethod={async (fileData: Kotilogi.FileType) => {
-                    await deleteFile(tablename, fileData.id);
-                }}/>
-            </Gallery.DeleteModal>
+      <Gallery.DeleteModal>
+        <DeleteSelectedItemsModal
+          deleteMethod={async (fileData: Kotilogi.FileType) => {
+            await deleteFile(tablename, fileData.id);
+          }}
+        />
+      </Gallery.DeleteModal>
 
-            <Gallery.Header title="Kuvat">
-                <Gallery.DeleteModalTrigger>
-                    <DeleteButton/>
-                </Gallery.DeleteModalTrigger>
+      <Gallery.Header title="Kuvat">
+        <Gallery.DeleteModalTrigger>
+          <DeleteButton />
+        </Gallery.DeleteModalTrigger>
 
-                <Gallery.AddModalTrigger>
-                    <AddButton/>
-                </Gallery.AddModalTrigger>
-            </Gallery.Header>
+        <Gallery.AddModalTrigger>
+          <AddButton />
+        </Gallery.AddModalTrigger>
+      </Gallery.Header>
 
-            <Gallery.Body displayStyle="horizontal" itemComponent={ImageComponent} errorElement={
-                <ImageError message="Et ole vielä lisännyt kohteelle kuvia. Aloita painamalla Lisää-Uusi painiketta."/>
-            }/>
-        </Gallery>
-    )
+      <Gallery.Body
+        displayStyle="horizontal"
+        itemComponent={ImageComponent}
+        errorElement={
+          <ImageError message="Et ole vielä lisännyt kohteelle kuvia. Aloita painamalla Lisää-Uusi painiketta." />
+        }
+      />
+    </Gallery>
+  );
 }
