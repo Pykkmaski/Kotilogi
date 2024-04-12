8d5c8a3d56d4339950203b34cab3cdaaec195dda
unknown <jens.osterberg@gmail.com>
2024-04-12 12:24:41 +0300

    Updated tos

--- app/(blackHeader)/tos/payments/page.tsx
+++ app/(blackHeader)/tos/payments/page.tsx
@@ -1,54 +1,114 @@
-const Heading = ({children}) => <h1 className="text-2xl font-semibold mb-4">{children}</h1>
-const SubHeading = ({children}) => <h2 className="text-xl font-semibold mb-4">{children}</h2>
+const Heading = ({ children }) => <h1 className='text-2xl font-semibold mb-4'>{children}</h1>;
+const SubHeading = ({ children }) => <h2 className='text-xl font-semibold mb-4'>{children}</h2>;
 const CustomLink = (props: React.ComponentProps<'a'>) => {
-    return <a {...props} className="text-orange-500">{props.href}</a>
-}
+  return (
+    <a {...props} className='text-orange-500'>
+      {props.href}
+    </a>
+  );
+};
 
-export default async function PaymentTermsPage(){
-    return (
-        <main className="text-slate-500 lg:px-32 xs:px-2 pt-8 h-full pb-10">
-            <Heading>Toimitusehdot</Heading>
+export default async function PaymentTermsPage() {
+  return (
+    <main className='text-slate-500 lg:px-32 xs:px-2 pt-8 h-full pb-10'>
+      <Heading>Toimitusehdot</Heading>
 
-            <p>
-                Kotidok Oy 3426507-4 (jäljempänä verkkokauppa) myy tuotteita yksityishenkilöille Suomeen.<br/> 
-                Pidätämme oikeuden toimitusehtojen ja hintojen muutoksiin. Tuotteiden hinnat sisältävät arvonlisäveron.<br/><br/>
-            </p>
-           
-            <SubHeading>Verkkokaupan yhteystiedot</SubHeading>
-            
-            <p>
-                Sähköposti: kotidok.service@gmail.com<br/>
-                Puhelin: 0451310749<br/>
-                Postiosoite: Tomintie 13, 65300 Vaasa<br/><br/>
-            </p>
-            
-            <SubHeading>Tilaaminen</SubHeading>
+      <p>
+        Kotidok Oy 3426507-4 (jäljempänä verkkokauppa) myy tuotteita yksityishenkilöille Suomeen.
+        <br />
+        Pidätämme oikeuden toimitusehtojen ja hintojen muutoksiin. Tuotteiden hinnat sisältävät arvonlisäveron.
+        <br />
+        <br />
+      </p>
 
-            <p>
-                Tilattavat tuotteet valitaan verkkosivuilla lisäämällä ne ostoskoriin. Tilaus lähetetään maksamalla ostoskorin sisältö verkkokaupan kassatoiminnossa.<br/> 
-                Tehdessäsi tilauksen hyväksyt nämä toimitusehdot, tuotteiden hinnat sekä toimituskulut. Mikäli tilaushetkellä annetaan sähköpostiosoite, tilauksesta lähetetään tilausvahvistus sähköpostitse.<br/>
-                Tilausvahvistuksesta ilmenevät tilatut tuotteet sekä hinta.<br/><br/>
-            </p>
-            
-            <SubHeading>Maksaminen</SubHeading>
+      <SubHeading>Verkkokaupan yhteystiedot</SubHeading>
 
-            <p>
-                Verkkokaupan maksuvälittäjänä toimii Visma Pay (Visma Payments Oy, y-tunnus 2486559-4), joka on rekisteröity Finanssivalvonnan ylläpitämään maksulaitosrekisteriin.<br/>
-                Maksamiseen siirrytään Visma Payn verkkopalvelun kautta ja tiliotteella ja laskulla maksun saajana näkyy Visma Pay tai Visma Payments Oy.<br/>
-                Visma Pay välittää maksut verkkokauppiaalle. Maksaminen on turvallista, sillä kaikki maksutapahtumaa koskevat tiedot välitetään salattua yhteyttä käyttäen niin ettei kukaan ulkopuolinen taho näe maksutapahtuman tietoja.<br/><br/>
+      <p>
+        Sähköposti: kotidok.service@gmail.com
+        <br />
+        Puhelin: 0451310749
+        <br />
+        Postiosoite: Timontie 13, 65300 Vaasa
+        <br />
+        <br />
+      </p>
 
-                Kauppa syntyy verkkokaupan asiakkaan ja verkkokaupan välille. Verkkokaupan vastuulla ovat kaikki kauppaan liittyvät velvoitteet.<br/><br/>
+      <SubHeading>Tilaaminen</SubHeading>
 
-                Lue lisää Visma Paysta: <CustomLink href="https://www.vismapay.fi/"/><br/><br/>
-            </p>
-        
-            <SubHeading>Maksutavat</SubHeading>
+      <p>
+        Tilattavat tuotteet lisätään automaattisesti seuraavien toimintojen yhteydessä ostoskoriin:
+        <br />
+        <br />
+        -Talon lisäys
+        <br />
+        -Talon uudelleen aktivointi, jos se on aiemmin poistettu käytöstä.
+        <br />
+        <br />
+        Jokaista taloa kohden lisätään vuosittain toistuva lasku, jonka hinta on sama kuin tämänhetkinen talon lisäysmaksu, joka on maksettava eräpäivään mennessä, tai talo
+        poistetaan käytöstä.
+        <br />
+        <br />
+        Tilaus lähetetään maksamalla ostoskorin sisältö verkkokaupan kassatoiminnossa.
+        <br />
+        <br />
+        Tehdessäsi tilauksen hyväksyt nämä toimitusehdot, tuotteiden hinnat sekä toimituskulut. Mikäli tilaushetkellä annetaan sähköpostiosoite, tilauksesta lähetetään
+        tilausvahvistus sähköpostitse.
+        <br />
+        Tilausvahvistuksesta ilmenevät tilatut tuotteet sekä hinta.
+        <br />
+        <br />
+        Kotidok ei suorita maksujen palautuksia.
+        <br />
+        <br />
+      </p>
 
-            <p>
-                Visma Pay -palvelun kautta voit maksaa verkkopankkitunnuksilla, lompakolla, maksukorteilla (credit/debit), laskulla tai osamaksulla. 
-                Käytettävissä ovat seuraavat maksutavat: Osuuspankki, Nordea, Danske Bank, Oma Säästöpankki, Säästöpankki, Aktia, Paikallisosuuspankit, S-Pankki, Handelsbanken, Ålandsbanken. <br/><br/>
-                {
-                    /**
+      <SubHeading>Maksaminen</SubHeading>
+
+      <p>
+        Verkkokaupan maksuvälittäjänä toimii Visma Pay (Visma Payments Oy, y-tunnus 2486559-4), joka on rekisteröity Finanssivalvonnan ylläpitämään maksulaitosrekisteriin.
+        <br />
+        Maksamiseen siirrytään Visma Payn verkkopalvelun kautta ja tiliotteella ja laskulla maksun saajana näkyy Visma Pay tai Visma Payments Oy.
+        <br />
+        Visma Pay välittää maksut verkkokauppiaalle. Maksaminen on turvallista, sillä kaikki maksutapahtumaa koskevat tiedot välitetään salattua yhteyttä käyttäen niin ettei kukaan
+        ulkopuolinen taho näe maksutapahtuman tietoja.
+        <br />
+        <br />
+        Kauppa syntyy verkkokaupan asiakkaan ja verkkokaupan välille. Verkkokaupan vastuulla ovat kaikki kauppaan liittyvät velvoitteet.
+        <br />
+        <br />
+        Lue lisää Visma Paysta: <CustomLink href='https://www.vismapay.fi/' />
+        <br />
+        <br />
+      </p>
+
+      <SubHeading>Maksutavat</SubHeading>
+
+      <p>
+        Visma Pay -palvelun kautta voit maksaa verkkopankkitunnuksilla, lompakolla, maksukorteilla (credit/debit), laskulla tai osamaksulla.
+        <br />
+        Käytettävissä ovat seuraavat maksutavat: <br />
+        <br />
+        Osuuspankki
+        <br />
+        Nordea
+        <br />
+        Danske Bank
+        <br />
+        Oma Säästöpankki
+        <br />
+        Säästöpankki
+        <br />
+        Aktia
+        <br />
+        Paikallisosuuspankit
+        <br />
+        S-Pankki
+        <br />
+        Handelsbanken
+        <br />
+        Ålandsbanken. <br />
+        <br />
+        {/**
                      * MobilePay: Voit maksaa MobilePay-lompakollasi mikäli olet sallinut verkkokaupoissa maksamisen sovelluksen asetuksista. <br/>
                         MobilePay-lompakolla suoritetut maksut veloitetaan lompakkoon liitetyltä maksukortilta. Mikäli maksun veloittaminen maksukortilta epäonnistuu, MobilePay-lompakolla maksaminen ei ole mahdollista verkkokaupassa.<br/><br/>
 
@@ -67,19 +127,23 @@ export default async function PaymentTermsPage(){
                         Alisa Yrityslasku on joustava ja turvallinen maksutapa yrityksille. Maksutapa sopii kaikille suomalaisille yrityksille yhtiömuodosta riippumatta.<br/>
                         Ostajan osto-oikeus varmennetaan kokonaan digitaalisesti ja ostotapahtuma vie vain muutaman minuutin. Yritys voi itse valita sopivan maksuajan kolmesta eri vaihtoehdosta: 14 päivää, 30 päivää tai 60 päivää.<br/>
                         Lisätietoja Alisa Yrityslaskuun liittyen löydät osoitteesta: <CustomLink href="https://www.alisapankki.fi/"/><br/><br/>
-                     */
-                }
-                
-            </p>
+                     */}
+      </p>
 
-            <SubHeading>Visma Pay -maksupalvelun yhteystiedot</SubHeading>
+      <SubHeading>Visma Pay -maksupalvelun yhteystiedot</SubHeading>
 
-            <p>
-                Visma Payments Oy (Y-tunnus 2486559-4)<br/>
-                Sähköposti: <a href="mailto:helpdesk@vismapay.com" className="text-orange-500">helpdesk@vismapay.com</a><br/>
-                Puhelin: 09 315 42 037 (arkisin klo 9-16)<br/>
-                Postiosoite: Brahenkatu 4, 53100 Lappeenranta
-            </p>
-        </main>
-    );
+      <p>
+        Visma Payments Oy (Y-tunnus 2486559-4)
+        <br />
+        Sähköposti:{' '}
+        <a href='mailto:helpdesk@vismapay.com' className='text-orange-500'>
+          helpdesk@vismapay.com
+        </a>
+        <br />
+        Puhelin: 09 315 42 037 (arkisin klo 9-16)
+        <br />
+        Postiosoite: Brahenkatu 4, 53100 Lappeenranta
+      </p>
+    </main>
+  );
 }
