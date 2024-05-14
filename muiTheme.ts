16c2a3e16c4ab77784e611ea71354f65ca4f1df6
unknown <jens.osterberg@gmail.com>
2024-05-14 19:35:26 +0300

    updated add property modal

---
+++ muiTheme.ts
@@ -0,0 +1,11 @@
+'use client';
+
+import { createTheme } from '@mui/material';
+import { green, grey, orange } from '@mui/material/colors';
+
+export const theme = createTheme({
+  palette: {
+    primary: orange,
+    secondary: grey,
+  },
+});
