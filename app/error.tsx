011f9f3b9035a08e1cb1ccc46d6f7f0ee0d7583b
Pykkmaski <kotidok.service@gmail.com>
2024-08-04 17:08:41 +0300

    Removed google ads script

--- app/error.tsx
+++ app/error.tsx
@@ -1,20 +1,17 @@
-"use client";
+'use client';
 
-export default function Error({error, reset}){
-    return (
-        <main className="flex flex-col flex-1 mx-32">
-            <div className="mt-32 flex flex-col text-xl">
-                <h1 className="text-4xl">Hups! Kohtasimme virheen!</h1>
-                <span className="text-slate-500 mt-4">
-                    Virheviesti: <span className="text-red-600 font-semibold">{error.message}</span>
-                </span>
-                
-            </div>
-            
-            <p className="mt-8">
-                Kokeile päivittää sivu.
-            </p>
-            {reset}
-        </main>
-    )
+export default function Error({ error, reset }) {
+  return (
+    <main className='flex flex-col flex-1 mx-32'>
+      <div className='mt-32 flex flex-col text-xl'>
+        <h1 className='text-4xl'>Hups! Kohtasimme virheen!</h1>
+        <span className='text-slate-500 mt-4'>
+          Virheviesti: <span className='text-red-600 font-semibold'>{error.message}</span>
+        </span>
+      </div>
+
+      <p className='mt-8'>Kokeile päivittää sivu.</p>
+      {reset}
+    </main>
+  );
 }
