19b99778be705975d0b839692314ddca8dd6b311
Pykkmaski <kotidok.service@gmail.com>
2024-08-11 16:54:53 +0300

    Added delete step page

---
+++ app/newDashboard/properties/[propertyId]/events/[eventId]/steps/[stepId]/delete/DeleteStepForm.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { ADeleteEventStep } from '@/actions/eventSteps';
+import { ADeleteProperty } from '@/actions/properties';
+import { FormBase } from '@/components/New/Forms/FormBase';
+import { ObjectDeletionForm } from '@/components/New/Forms/ObjectDeletionForm';
+import { MainHeading } from '@/components/New/Typography/Headings';
+import { FormControl, Input } from '@/components/UI/FormUtils';
+import { FormStatus, useDataSubmissionForm } from '@/hooks/useDataSubmissionForm';
+import { useInputData } from '@/hooks/useInputData';
+import { Delete } from '@mui/icons-material';
+import { Button } from '@mui/material';
+import {
+  AppartmentDataType,
+  EventStepDataType,
+  HouseDataType,
+  PropertyDataType,
+} from 'kotilogi-app/models/types';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+
+type DeletePropertyFormProps = {
+  step: EventStepDataType;
+};
+
+export function DeleteStepForm({ step }: DeletePropertyFormProps) {
+  return (
+    <ObjectDeletionForm
+      returnUrl='/newDashboard/properties'
+      objectId={step.id}
+      deleteMethod={async data => {
+        return await ADeleteEventStep(step.id).then(res => {
+          if (res == -1) {
+            toast.error('Talon voi poistaa ainoastaan sen omistaja!');
+          } else {
+            toast.success('Talo poistettu!');
+          }
+          return res;
+        });
+      }}>
+      <MainHeading>Poista vaihe</MainHeading>
+      <p className='text-lg mb-4'>Olet poistamassa vaihetta ${step.title}. Oletko varma?</p>
+    </ObjectDeletionForm>
+  );
+}
---
+++ app/newDashboard/properties/[propertyId]/events/[eventId]/steps/[stepId]/delete/page.tsx
@@ -0,0 +1,22 @@
+import { Main } from '@/components/New/Main';
+import db from 'kotilogi-app/dbconfig';
+import { loadSession } from 'kotilogi-app/utils/loadSession';
+import { DeleteStepForm } from './DeleteStepForm';
+
+export async function DeleteEventStepPage({ params }) {
+  const stepId = params.stepId;
+  const [step] = await db('data_propertyEventSteps')
+    .join('data_objects', { 'data_objects.id': 'data_propertyEventSteps.id' })
+    .where({ id: stepId });
+
+  const session = await loadSession();
+  const authorized = session.user.id == step.authorId;
+
+  return !authorized ? (
+    <span>Vain vaiheen luoja voi poistaa sen!</span>
+  ) : (
+    <Main>
+      <DeleteStepForm step={step} />
+    </Main>
+  );
+}
