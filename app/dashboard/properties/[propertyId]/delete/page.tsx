0079e46de0d489890a4e6ed09fb1e87a89224e83
Pykkmaski <kotilogi.service@gmail.com>
2024-09-09 16:32:26 +0300

    Updated event server actions

--- app/dashboard/properties/[propertyId]/delete/page.tsx
+++ app/dashboard/properties/[propertyId]/delete/page.tsx
@@ -1,9 +1,4 @@
-import { FormBase } from '@/components/New/Forms/FormBase';
 import { Main } from '@/components/New/Main';
-import { MainHeading } from '@/components/New/Typography/Headings';
-import { FormControl, Input } from '@/components/UI/FormUtils';
-import { Check, Delete } from '@mui/icons-material';
-import { Button } from '@mui/material';
 import { redirect } from 'next/navigation';
 import { DeletePropertyForm } from './_components/DeletePropertyForm';
 import db from 'kotilogi-app/dbconfig';
@@ -12,6 +7,9 @@ import { loadSession } from 'kotilogi-app/utils/loadSession';
 export default async function DeletePropertyPage({ params, searchParams }) {
   const [property] = await db('data_properties').where({ id: params.propertyId });
   const session = await loadSession();
+  if (!session) {
+    redirect('/login');
+  }
   const [owner] = await db('data_propertyOwners').where({
     userId: session.user.id,
     propertyId: property.id,
--- app/dashboard/properties/[propertyId]/events/add/_components/actions.ts
+++ app/dashboard/properties/[propertyId]/events/add/_components/actions.ts
@@ -2,13 +2,17 @@
 
 import { revalidatePath } from 'kotilogi-app/app/api/_utils/revalidatePath';
 import { createEvent, updateEvent } from 'kotilogi-app/dataAccess/events';
+import { EventDataType } from 'kotilogi-app/dataAccess/types';
+import { z } from 'zod';
 
-export const runUpdate = async (eventId, data) => {
+export const runUpdate = async (eventId: string, data: Partial<EventDataType>) => {
+  z.string().parse(eventId);
   await updateEvent(eventId, data);
   revalidatePath('/dashboard/properties/[propertyId]');
 };
 
-export const onSubmit = async (propertyId, data, files) => {
+export const onSubmit = async (propertyId: string, data: EventDataType, files) => {
+  z.string().parse(propertyId);
   await createEvent({ ...data, parentId: propertyId });
   revalidatePath('/dashboard/properties/[propertyId]');
 };
