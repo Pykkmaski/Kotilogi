306bcc611fba98dc964f092dfb226c82b6b0cad6
Pykkmaski <kotilogi.service@gmail.com>
2024-09-12 00:09:01 +0300

    Updated index page

--- app/(transparentHeader)/(index)/_components/new/Hero.tsx
+++ app/(transparentHeader)/(index)/_components/new/Hero.tsx
@@ -48,7 +48,7 @@ const Separator = () => {
 };
 export const Hero = () => {
   return (
-    <section className='lg:h-[1080px] xs:h-[720px] lg:px-48 xs:px-4 flex flex-col relative lg:items-start py-8 bg-black justify-center'>
+    <section className='lg:h-[1080px] xs:h-[720px] lg:px-48 xs:px-4 flex flex-col relative lg:items-start py-8 bg-black justify-center bg-[url("/hero_kitchen_background.jpg")] bg-cover'>
       <img
         src='/hero_background_shape.png'
         className='absolute top-0 left-0 z-10 xs:hidden lg:block'
@@ -56,10 +56,10 @@ export const Hero = () => {
 
       <img
         src='/hero_kitchen_background.jpg'
-        className='absolute top-0 right-0 lg:block xs:hidden'
+        className='absolute top-0 right-0 lg:hidden xs:hidden'
       />
 
-      <div className='flex flex-col gap-6 z-[10] lg:items-start xs:items-center'>
+      <div className='flex flex-col gap-6 z-[10] lg:items-start xs:items-center xs:bg-[#0005] lg:bg-none xs:py-4 lg:py-0'>
         <HeroText />
         <div className='flex flex-col gap-[26px]'>
           <RegisterButton />
--- app/(transparentHeader)/layout.tsx
+++ app/(transparentHeader)/layout.tsx
@@ -42,14 +42,14 @@ export default function TransparentHeaderLayout({ children }: React.PropsWithChi
   };
   return (
     <div className='flex flex-col bg-white h-screen'>
-      <header className='absolute flex w-full items-center justify-between border-b-white border-b-[1px] py-4 lg:px-48 xs:px-4 z-[20]'>
+      <header className='absolute flex w-full items-center justify-between lg:border-b-white xs:border-b-black border-b-[1px] py-4 lg:px-48 xs:px-4 z-[20]'>
         <Link
           href='/'
-          className='lg:text-2xl xs:text-xl font-bold text-white flex gap-2 items-baseline hover:no-underline'>
+          className='lg:text-2xl xs:text-xl font-bold lg:text-white xs:text-black flex gap-2 items-baseline hover:no-underline'>
           KOTIDOK
           <small className='text-xs'>BETA</small>
         </Link>
-        <nav className='lg:text-lg xs:text-base text-white font-semibold'>
+        <nav className='lg:text-lg xs:text-base lg:text-white xs:text-black font-semibold'>
           <ul className='flex lg:gap-8 xs:gap-4 items-center'>
             {getLinks().map(({ href, text }) => (
               <Link href={href}>{text}</Link>
