4574e821f5d008abc0cdbddbd6cfe1c176d0244a
Pykkmaski <kotilogi.service@gmail.com>
2024-09-11 12:00:25 +0300

    Deleted unused test

--- app/dashboard/properties/add/__tests__/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Page from '../page';
-
-describe('Testing the property add-page', () => {
-  beforeAll(() => {
-    render(<Page />);
-  });
-});
