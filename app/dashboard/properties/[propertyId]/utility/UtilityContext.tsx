db77d4cf136e93e5b496d4b97d44a3f745c8c9e2
Pykkmaski <kotilogi.service@gmail.com>
2024-09-14 16:43:21 +0300

    Fixed build errors

--- app/dashboard/properties/[propertyId]/utility/UtilityContext.tsx
+++ app/dashboard/properties/[propertyId]/utility/UtilityContext.tsx
@@ -9,7 +9,7 @@ type UtilityProviderProps = {
   selectedTypes: string[];
   allTypes?: string[];
   year: null | number;
-  years: number[];
+  years?: number[];
 };
 
 const UtilityContext = createContext<UtilityProviderProps>(null);
