12f2cf5dbfefde6ef88f6aafeacc0b7f97f255bb
Pykkmaski <kotilogi.service@gmail.com>
2024-09-16 23:20:11 +0300

    Updated main layout background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }: React.PropsWithChildren) {
-  const bodyClassName = ['flex flex-col min-h-screen bg-gradient-to-b from-slate-50 to-slate-200'];
+  const bodyClassName = ['flex flex-col min-h-screen bg-gradient-to-b from-gray-50 to-gray-100'];
 
   return (
     <html lang='en'>
--- components/New/Paper.tsx
+++ components/New/Paper.tsx
@@ -1,5 +1,3 @@
 export function Paper({ children }) {
-  return (
-    <div className='bg-gray-50 shadow-lg flex-1 w-full pt-2 lg:px-4 xs:px-2 pb-16'>{children}</div>
-  );
+  return <div className='flex-1 w-full pt-2 lg:px-4 xs:px-2 pb-16'>{children}</div>;
 }
