4a327c9738db1ba1c1639df494a515a881d57b68
Pykkmaski <kotilogi.service@gmail.com>
2025-03-06 13:42:07 +0200

    Added google ads script

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,10 @@ window.dataLayer = window.dataLayer || [];
           rel='manifest'
           href='/manifest.json'
         />
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3009159750698563'
+          crossOrigin='anonymous'></script>
 
         {getAnalyticsScript()}
       </head>
